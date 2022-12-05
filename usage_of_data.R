44cf11168672069b1877a7c80b391560b7c7fca2
havishaJ <111279180+havishaJ@users.noreply.github.com>
2022-12-05 16:49:35 +0400

    Add files via upload

---
+++ usage_of_data.R
@@ -0,0 +1,44 @@
+#' Usage of Data
+
+#' Data Input- File name: file1
+#' Tab - GM (1, 1) model
+#' Tab - Improved background values
+
+
+#' Data Input- File name: file2
+#' Tab - Extended Forms
+#' Model - VSSGM (1, 1)
+
+
+#' Data Input - file names: file3.1 (in-sample) & file3.2 (out-sample)
+#' Tab - Extended Forms
+#' Model - EGM (1, 1) model
+
+
+#' Data Input: file name: file4
+#' Tab - Combined Models
+#' Model - NGBM (1, 1) model & TFDGM (1, 1) model
+
+
+#' Data Input: file name: file5
+#' Tab - Optimization
+
+
+#' Data Input - file names: file6.1 (in-sample) & file6.2 (out-sample)
+#' Tab - Residual Modification
+#' Model - Remnant GM (1, 1) model & TGM (1, 1) model
+
+
+#' Data Input: file name: file7
+#' Tab - Multivariable Sequences
+#' Models - GM (1, 3) model & IGM (1, 3) model
+
+
+#' Data Input: file name: file8
+#' Tab - Multivariable Interval Sequence
+#' Model - IG-NDGM (1, 2) model
+
+
+#' Data Input - file names: file9.1 (in-sample) & file9.2 (out-sample)
+#' Tab - Multivariable Interval Sequence
+#' Model - MDBGM (1, 2) model
